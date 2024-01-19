6e8b118d96126811c452e7e321030e42b313bac7
Alex Tetervak <tetervak@sheridancollege.ca>
2024-01-19 14:43:50 -0500

    received the data as one object

--- src/main/java/ca/tetervak/hellouser/controller/HelloUserController.java
+++ src/main/java/ca/tetervak/hellouser/controller/HelloUserController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -23,13 +24,10 @@ public class HelloUserController {
 
     @GetMapping("/output")
     public String output(
-            @RequestParam String firstName,
-            @RequestParam String lastName,
+            @ModelAttribute AppUser user,
             Model model){
 
         log.trace("called output()");
-
-        var user = new AppUser(firstName, lastName);
 
         log.debug("user = " + user);
 
