d99e4238dd70cd6985e1f1039ef279469fe5ce3e
Alex Tetervak <tetervak@sheridancollege.ca>
2024-01-19 16:00:27 -0500

    fixed a warning

--- src/main/java/ca/tetervak/hellouser/controller/HelloUserController.java
+++ src/main/java/ca/tetervak/hellouser/controller/HelloUserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloUserController {
 
-    private Logger log = LoggerFactory.getLogger(HelloUserController.class);
+    private final Logger log = LoggerFactory.getLogger(HelloUserController.class);
 
     @GetMapping("/")
     public String input(){
