6ba6e7664b6437cb54af2901e90ed94117afc90c
Alex Tetervak <tetervak@sheridancollege.ca>
2024-01-19 13:54:57 -0500

    used the model parameter

--- src/main/java/ca/tetervak/hellouser/controller/HelloUserController.java
+++ src/main/java/ca/tetervak/hellouser/controller/HelloUserController.java
@@ -1,18 +1,20 @@
 package ca.tetervak.hellouser.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloUserController {
 
     @GetMapping("/")
-    public ModelAndView output(){
+    public String output(Model model){
 
         var userName = "Jane";
 
-        return new ModelAndView("Output", "name", userName);
+        model.addAttribute("name", userName);
+
+        return "Output";
     }
 
 }
